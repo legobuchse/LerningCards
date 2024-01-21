493e75fb22d5871466aa94bb5d59ecaad38eb6e0
simon <herreisen@gmx.de>
2024-01-21 22:29:24 +0100

    create and load json files

--- LearningCards.py
+++ LearningCards.py
@@ -2,18 +2,24 @@ import json
 import time
 
 
+totalPointsReached = 0
+totalPointsPossible = 0
 
 def startTest(text):
     print(text)
 
-def loadJsonFromFile():
-    with open("cards.json", "r") as f:
+def loadJsonFromFile(fileName):
+    with open(fileName, "r") as f:
         d = json.load(f)
     print(d)
     print(d.items())
     print("-----")
     print("Completed loading")
     return d
+
+def writeJsonToFile(fileName, content):
+    with open(fileName, "w") as f:
+        json.dump(content, f, indent="", ensure_ascii=False)
 
 def testReturn():
     return 'test'
@@ -25,14 +31,68 @@ def loadJsonFromString():
     data = json.loads('{"question" : "Test Question Text", "answer" : "Test Answer Text"}')
     print(data[0])
 
+def checkAnswer(cAns, uAns):
+
+    #answers = uAns.split(uAns, ";", 100)
+    correct = 0
+    wrong = 0
+
+    for x in cAns:
+        if(uAns.__str__().find(x["answer"]) < 0):
+            wrong += 1
+        else:
+            correct += 1
+            print(x["answer"] + " is correct!")
+
+    global totalPointsReached
+    totalPointsReached += correct
+    global totalPointsPossible
+    totalPointsPossible += len(cAns)
+    print("You got " + correct.__str__() + " correct and " + wrong.__str__() + " wrong answer(s)")
+    print("Possible answer(s): ")
+    for a in cAns:
+        print(a["answer"])
+
 def startQuestions():
     print("Get ready to answer!")
-    cards = loadJsonFromFile()
+    fileName = input("Json file name: ")
+    cards = loadJsonFromFile(fileName)
     for x in cards["cards"]:
         print(x["question"])
+        checkAnswer(x["answers"], input("Answer: "))
+        time.sleep(1)
+    print("You finished all cards!")
+    print("Points: " + totalPointsReached.__str__() + " / " + totalPointsPossible.__str__())
 
 def createNewCard():
     print("Create new card")
+    fileName = input("File name: ")
+    cards = {"cards":[]}
+    count = 0
+    addCard = True
+    while addCard:
+        count += 1
+        card = {"number":"testNumber","question":"testQuestion","answers":[]}
+        card["number"] = count
+        card["question"] = input("Question: ")
+        addAnswer = True
+        answers = []
+        #answerObj = {"answer":"testAnswer"}
+        while addAnswer:
+            a = {"answer":"testAnswer"}
+            a["answer"] = input("Answer: ")
+            answers.append(a)
+            print("Added answer: " + a.__str__())
+            if(input("Add another answer? (y/n) ") == "n"):
+                addAnswer = False
+        card["answers"] = answers
+        cards["cards"].append(card)
+        print("Added card: " + card.__str__())
+        if(input("Add another card? (y/n) ") == "n"):
+            addCard = False
+    print("Created " + count.__str__() + " card(s):")
+    print(cards)
+    writeJsonToFile(fileName ,cards)
 
 print("Welcome to Learning Cards")
 
